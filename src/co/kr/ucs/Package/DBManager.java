7731ce4bba1aa8b0e48ee682eaaaa2ddcbd63b22
LEEHYEJIN7 <wideopenspace9@gmail.com>
2017-11-25 11:12:08 +0900

    화면설계 3.1 Service.java - 파일 생성, DBManager.java 작성

---
+++ src/co/kr/ucs/Package/DBManager.java
@@ -0,0 +1,58 @@
+package co.kr.ucs.Package;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBManager {
+	//DB연결
+		private static Connection getConnection(){
+			Connection conn=null;
+			
+			try{
+				String url="jdbc:oracle:thin:@220.76.203.39:1521:UCS";
+				String id="UCS_STUDY";
+				String pw="qazxsw";
+				
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				conn=DriverManager.getConnection(url,id,pw);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			
+			return conn;
+		}
+		
+		//close : Connection, PreparedStatement, ResultSet close
+		public static void close(Connection conn){
+			if(conn!=null){
+				try{
+					conn.close();
+				}catch(SQLException e){
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		public static void close(PreparedStatement pstmt){
+			if(pstmt!=null){
+				try{
+					pstmt.close();
+				}catch(SQLException e){
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		public static void close(ResultSet rs){
+			if(rs!=null){
+				try{
+					rs.close();
+				}catch(SQLException e){
+					e.printStackTrace();
+				}
+			}
+		}
+}
---
+++ src/co/kr/ucs/Service/BoardService.java
@@ -0,0 +1,5 @@
+package co.kr.ucs.Service;
+
+public class BoardService {
+
+}
